4f6b588b9e46befb904ca1dfed907024d0511473
Lucas Marins <lucasmarinsramalho@gmail.com>
2022-10-24 22:16:45 -0300

    apagando o trem que eu esqueci de tirar

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lucas/Documentos/teoria-dos-grafos
-# Build directory: /home/lucas/Documentos/teoria-dos-grafos/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
